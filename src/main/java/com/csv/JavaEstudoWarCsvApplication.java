0762d3738cdec2dd900597c45124f9dd0c86ba08
ricardo-dev <ricardopuc.computacao@gmail.com>
2022-02-15 17:29:04 -0300

    [UP] update

--- src/main/java/com/csv/JavaEstudoWarCsvApplication.java
+++ src/main/java/com/csv/JavaEstudoWarCsvApplication.java
@@ -10,5 +10,4 @@ public class JavaEstudoWarCsvApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEstudoWarCsvApplication.class, args);
 	}
-	
 }
